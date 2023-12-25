e5be7fdfc5c7b132c0abe3f8f0ad545f6ded1e4c
dr-sanna <quentinsanna.66@gmail.com>
2023-12-25 11:44:31 +0100

    commit message

--- src/LiensUtilesComponent.js
+++ src/LiensUtilesComponent.js
@@ -4,9 +4,9 @@ const LiensUtilesComponent = ({ liens }) => {
     return (
         <div className="list-group">
             {liens.map(lien => (
-                <a key={lien.id} href={lien.URL} target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">
-                    <h4 className="list-group-item-heading">{lien.Titre}</h4>
-                    <p className="list-group-item-text">{lien.Description}</p>
+                <a key={lien.id} href={lien.url} target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">
+                    <h4 className="list-group-item-heading">{lien.titre}</h4>
+                    <p className="list-group-item-text">{lien.description}</p>
                 </a>
             ))}
         </div>
