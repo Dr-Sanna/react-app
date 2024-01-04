691d28e7bb3510e3519be4635b04a65e189d28ec
dr-sanna <quentinsanna.66@gmail.com>
2024-01-04 01:22:21 +0100

    commit message

--- src/CasCardComponent.js
+++ src/CasCardComponent.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import MaterialCard from './MaterialCard';
+import { server } from './config';
 
 const CasCardComponent = React.memo(({ cas, onSelection }) => {
-  const baseUrl = process.env.REACT_APP_STRAPI_URL || ''; // Assurez-vous que cette variable est correctement définie.
-
   // Utilisez l'URL de base de l'image
   const imageUrl = cas?.attributes?.image?.data?.attributes?.url
-    ? `${baseUrl}${cas.attributes.image.data.attributes.url}`
+    ? `${server}${cas.attributes.image.data.attributes.url}`
     : '/defaultImage.jpg'; // Image par défaut si aucune n'est trouvée.
 
   return (
