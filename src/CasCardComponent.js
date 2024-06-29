c520286ab68c057673866d0f7c51826b0f241329
dr-sanna <quentinsanna.66@gmail.com>
2024-06-29 23:11:55 +0200

    commit message

--- src/CasCardComponent.js
+++ src/CasCardComponent.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { motion } from 'framer-motion';
 import { Card, CardContent, CardMedia } from '@mui/material';
 import styled from 'styled-components';
 import { server } from './config';
 import { useLocation } from 'react-router-dom';
 import { CustomToothLoader } from './CustomToothLoader';
+import './CasCardComponent.css'; // Import the CSS for opacity
 
 const CasCardContainer = styled.div`
   display: grid;
@@ -62,7 +62,7 @@ const CasCardComponent = ({ casCliniques, onSelection }) => {
   useEffect(() => {
     observerRef.current = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
-        if (entry.isIntersecting) {
+        if (entry.isIntersecting && entry.target.classList.contains('image-loaded')) {
           const index = Number(entry.target.dataset.index);
           setImageDisplayedStates(prevStates => {
             const newStates = [...prevStates];
@@ -72,8 +72,13 @@ const CasCardComponent = ({ casCliniques, onSelection }) => {
           observerRef.current.unobserve(entry.target);
         }
       });
-    });
-    return () => observerRef.current.disconnect();
+    }, { threshold: 1.0 }); // Ensure the image is fully in view
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
   }, []);
 
   const handleImageLoad = (index) => {
@@ -85,19 +90,20 @@ const CasCardComponent = ({ casCliniques, onSelection }) => {
   };
 
   const registerImageRef = (index, node) => {
-    if (node) {
+    if (node && observerRef.current) {
       observerRef.current.observe(node);
     }
   };
 
   return (
-    <CasCardContainer>
-      {!allImagesLoaded ? (
-        <div style={{ gridColumn: '1 / -1', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div>
+      {!allImagesLoaded && (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
           <CustomToothLoader />
         </div>
-      ) : (
-        casCliniques.map((cas, index) => {
+      )}
+      <CasCardContainer className={!allImagesLoaded ? 'hidden' : ''}>
+        {casCliniques.map((cas, index) => {
           const imageUrl = cas?.attributes?.image?.data?.attributes?.url
             ? `${server}${cas.attributes.image.data.attributes.url}`
             : '/defaultImage.jpg';
@@ -105,64 +111,60 @@ const CasCardComponent = ({ casCliniques, onSelection }) => {
 
           return (
             <div key={cas.id} style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-              <motion.div whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 500, damping: 15 }}>
-                <a href={urlPourPrevisualisation} onClick={(e) => { e.preventDefault(); onSelection(cas); }}>
-                  <Card sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
+              <a href={urlPourPrevisualisation} onClick={(e) => { e.preventDefault(); onSelection(cas); }}>
+                <Card sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '100%',
+                  height: '0',
+                  paddingTop: '75%',
+                  position: 'relative',
+                  bgcolor: 'white',
+                  borderRadius: '10px',
+                  boxShadow: '0 1px 3px 0 rgba(0,0,0,0.2), 0 3px 4px -2px rgba(0,0,0,0.2)'
+                }}>
+                  <div
+                    className={imageLoadedStates[index] ? 'image-loaded' : 'image-loading'}
+                    style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                  >
+                    <CardMedia
+                      component="img"
+                      image={imageUrl}
+                      alt={cas?.attributes?.titre || 'Titre inconnu'}
+                      style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }}
+                      onLoad={() => handleImageLoad(index)}
+                      data-index={index}
+                      ref={node => registerImageRef(index, node)}
+                    />
+                  </div>
+                  <CardContent style={{
+                    position: 'absolute',
+                    bottom: 0,
                     width: '100%',
-                    height: '0',
-                    paddingTop: '75%',
-                    position: 'relative',
-                    bgcolor: 'white',
-                    borderRadius: '10px',
-                    boxShadow: '0 1px 3px 0 rgba(0,0,0,0.2), 0 3px 4px -2px rgba(0,0,0,0.2)'
+                    height: '60px',
+                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                    textAlign: 'center',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '5px',
                   }}>
-                    <motion.div
-                      initial={{ opacity: 0, filter: 'blur(10px)' }}
-                      animate={{ opacity: imageLoadedStates[index] ? 1 : 0, filter: imageLoadedStates[index] ? 'blur(0px)' : 'blur(10px)' }}
-                      transition={{ duration: 0.5 }}
-                      style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                    >
-                      <CardMedia
-                        component="img"
-                        image={imageUrl}
-                        alt={cas?.attributes?.titre || 'Titre inconnu'}
-                        style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '10px' }}
-                        onLoad={() => handleImageLoad(index)}
-                        data-index={index}
-                        ref={node => registerImageRef(index, node)}
-                      />
-                    </motion.div>
-                    <CardContent style={{
-                      position: 'absolute',
-                      bottom: 0,
+                    <h6 style={{
+                      margin: '0',
                       width: '100%',
-                      height: '60px',
-                      backgroundColor: 'rgba(255, 255, 255, 0.7)',
-                      textAlign: 'center',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      padding: '5px',
+                      overflow: 'hidden',
+                      whiteSpace: 'normal',
                     }}>
-                      <h6 style={{
-                        margin: '0',
-                        width: '100%',
-                        overflow: 'hidden',
-                        whiteSpace: 'normal',
-                      }}>
-                        {cas?.attributes?.titre || 'Titre inconnu'}
-                      </h6>
-                    </CardContent>
-                  </Card>
-                </a>
-              </motion.div>
+                      {cas?.attributes?.titre || 'Titre inconnu'}
+                    </h6>
+                  </CardContent>
+                </Card>
+              </a>
             </div>
           );
-        })
-      )}
-    </CasCardContainer>
+        })}
+      </CasCardContainer>
+    </div>
   );
 };
 
