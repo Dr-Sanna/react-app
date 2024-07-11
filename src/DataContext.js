3e13315e86bc52a5ff2cd8010982b6685f6abd57
dr-sanna <quentinsanna.66@gmail.com>
2024-07-12 01:34:14 +0200

    commit message

--- src/DataContext.js
+++ src/DataContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect, useCallback } from 'react';
 import { fetchMatieres, fetchSousMatieres, fetchCasCliniques, fetchCoursData } from './api';
-import { preloadImage, preloadImages } from './utils';
+import { preloadImage } from './utils';
 
 export const DataContext = createContext();
 
@@ -11,30 +11,6 @@ export const DataProvider = ({ children }) => {
   const [cours, setCours] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isCoursLoading, setIsCoursLoading] = useState(false);
-
-  const preloadNestedTestImages = async (test) => {
-    if (test) {
-      const testImageUrl = test.image ? test.image.data.attributes.url : '';
-      if (testImageUrl) {
-        await preloadImage(testImageUrl);
-      }
-
-      // Preload images in the nested test of test components
-      if (Array.isArray(test.test)) {
-        await Promise.all(test.test.map(async (nestedTestItem) => {
-          const deeperNestedImageUrl = nestedTestItem.image ? nestedTestItem.image.data.attributes.url : '';
-          if (deeperNestedImageUrl) {
-            await preloadImage(deeperNestedImageUrl);
-          }
-
-          // Recursively preload images in further nested tests
-          if (nestedTestItem.test) {
-            await preloadNestedTestImages(nestedTestItem);
-          }
-        }));
-      }
-    }
-  };
 
   const fetchData = useCallback(async () => {
     try {
@@ -51,9 +27,27 @@ export const DataProvider = ({ children }) => {
 
       // Précharger les images des cas cliniques
       await Promise.all(casCliniquesData.map(async (item) => {
+        const imageUrl = item.attributes.image ? item.attributes.image.data.attributes.url : '';
+        if (imageUrl) {
+          await preloadImage(imageUrl);
+        }
+
         // Preload images in the nested test components
         if (item.attributes.test) {
-          await preloadNestedTestImages(item.attributes.test);
+          const testImageUrl = item.attributes.test.image ? item.attributes.test.image.data.attributes.url : '';
+          if (testImageUrl) {
+            await preloadImage(testImageUrl);
+          }
+
+          // Preload images in the nested test of test components
+          if (Array.isArray(item.attributes.test.test)) {
+            await Promise.all(item.attributes.test.test.map(async (nestedTestItem) => {
+              const deeperNestedImageUrl = nestedTestItem.image ? nestedTestItem.image.data.attributes.url : '';
+              if (deeperNestedImageUrl) {
+                await preloadImage(deeperNestedImageUrl);
+              }
+            }));
+          }
         }
       }));
 
@@ -73,8 +67,27 @@ export const DataProvider = ({ children }) => {
 
       // Précharger les images des cours
       await Promise.all(coursData.map(async (item) => {
+        const imageUrl = item.attributes.image ? item.attributes.image.data.attributes.url : '';
+        if (imageUrl) {
+          await preloadImage(imageUrl);
+        }
+
+        // Preload images in the nested test components
         if (item.attributes.test) {
-          await preloadNestedTestImages(item.attributes.test);
+          const testImageUrl = item.attributes.test.image ? item.attributes.test.image.data.attributes.url : '';
+          if (testImageUrl) {
+            await preloadImage(testImageUrl);
+          }
+
+          // Preload images in the nested test of test components
+          if (Array.isArray(item.attributes.test.test)) {
+            await Promise.all(item.attributes.test.test.map(async (nestedTestItem) => {
+              const deeperNestedImageUrl = nestedTestItem.image ? nestedTestItem.image.data.attributes.url : '';
+              if (deeperNestedImageUrl) {
+                await preloadImage(deeperNestedImageUrl);
+              }
+            }));
+          }
         }
       }));
 
