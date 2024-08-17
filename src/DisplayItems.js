3694f58a8289730268979d5c0e4ae060af63ac8b
dr-sanna <quentinsanna.66@gmail.com>
2024-08-17 17:38:01 +0200

    commit message

--- src/DisplayItems.js
+++ src/DisplayItems.js
@@ -18,34 +18,79 @@ const DisplayItems = ({ items, onClickItem }) => {
   };
 
   return (
-    <div style={{ padding: '15px 60px', margin: 0, maxWidth: '100%' }}>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', margin: 0 }}>
+    <div 
+    className="item-menu-container">
+      <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'center',
+          margin: '-10px 0', // Assure un espacement uniforme
+        }}
+      >
         {sortedItems.map(item => (
           <div
+          className="item-menu"
             style={{
               ...getItemStyle(),
               textAlign: 'center',
-              marginTop: '16px',
+              margin: '10px', // Espacement entre les cases
               boxSizing: 'border-box',
-              padding: '0 15px'
+              padding: '0px',
+              borderRadius: '5px',
+              width: '180px', // Taille fixe des cases
+              height: '180px', // Hauteur fixe des cases
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between', // Espace entre l'image et le texte
             }}
             key={item.id}
             onClick={() => onClickItem(item)}
           >
-            <div className="circle-icon" style={{ position: 'relative', width: '100%', height: '100px' }}>
+            <div
+              className="image-menu"
+              style={{
+                width: '100%',
+                height: '130px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: '5px',
+              }}
+            >
               {item.attributes.image && item.attributes.image.data && (
                 <img
                   src={item.attributes.image.data.attributes.url}
                   alt={item.attributes.titre}
+                  style={{
+                    maxHeight: '100%',
+                    maxWidth: '100%',
+                  }}
                 />
               )}
             </div>
-            <p>{item.attributes.titre}</p>
+            <div
+              className="text-menu"
+              style={{
+                flex: 1, // Assure que le texte prend l'espace restant
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                lineHeight: '1.1',
+                
+              }}
+            >
+              <p style={{ margin: 0 }}>
+                {item.attributes.titre}
+              </p>
+            </div>
           </div>
         ))}
       </div>
     </div>
   );
+  
+  
 };
 
 export default DisplayItems;
