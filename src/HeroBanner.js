1cd1ef86eb5cba1a905c0a65aaa689afd202b4fd
dr-sanna <quentinsanna.66@gmail.com>
2024-09-28 04:38:57 +0200

    commit message

--- src/HeroBanner.js
+++ src/HeroBanner.js
@@ -5,9 +5,9 @@ import './HeroBanner.css';
 const HeroBanner = () => {
   return (
     <div className="hero-banner">
-      <h1>Bienvenue sur votre plateforme dentaire</h1>
+      <h1>Dr Sanna Randomizer</h1>
       <p>
-        Découvrez des cas cliniques passionnants, accédez à notre application de randomisation,
+        Découvrez des cas cliniques passionnants, accédez à une application de randomisation,
         explorez une documentation complète et trouvez des liens utiles pour enrichir votre pratique.
       </p>
     </div>
--- src/HomePage.js
+++ src/HomePage.js
@@ -7,24 +7,24 @@ import Matiere from './Matiere';
 import LiensUtilesWithData from './LiensUtilesWithData';
 import CasCliniquesComponent from './CasCliniquesComponent';
 import CoursComponent from './CoursComponent';
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { toUrlFriendly } from './config';
 import CoursDetailLoader from './CoursDetailLoader';
 import { CustomToothLoader } from './CustomToothLoader';
 import MainSections from './MainSections';
 import HeroBanner from './HeroBanner';
 // import RandomisationComponent from './RandomisationComponent'; // Si vous avez ce composant
+import BackgroundWrapper from './BackgroundWrapper'; // Importation du BackgroundWrapper
 
 const HomePage = ({ fontSize }) => {
   const { matieres, sousMatieres, isLoading } = useContext(DataContext);
   const navigate = useNavigate();
-  const location = useLocation();
   const [initialLoad, setInitialLoad] = useState(true);
 
   const handleMatiereClick = (matiere) => {
     const matiereTitleUrl = toUrlFriendly(matiere.attributes.titre);
     navigate(`/${matiereTitleUrl}`);
-  };  
+  };
 
   const getComponentByActionType = useMemo(
     () => (actionType) => {
@@ -49,34 +49,61 @@ const HomePage = ({ fontSize }) => {
   }, [isLoading, matieres]);
 
   return (
-    <div id="__docusaurus_skipToContent_fallback" className="main-wrapper mainWrapper_PEsc">
-      {location.pathname === '/' && <HeroBanner />}
+    <div
+      id="__docusaurus_skipToContent_fallback"
+      className="main-wrapper mainWrapper_PEsc"
+    >
+      {/* Supprimez le HeroBanner d'ici pour éviter la duplication */}
+      {/* {location.pathname === '/' && <HeroBanner />} */}
       <Routes>
         {/* Page d'accueil */}
         <Route
           path="/"
           element={
-            initialLoad ? (
-              <CustomToothLoader />
-            ) : (
-              <MainSections />
-            )
+            <BackgroundWrapper>
+              <HeroBanner />
+              {initialLoad ? (
+                <CustomToothLoader />
+              ) : (
+                <MainSections />
+              )}
+            </BackgroundWrapper>
           }
         />
         {/* Page de documentation */}
         <Route
           path="/documentation"
           element={
-            matieres ? (
-              <DisplayItems items={matieres} onClickItem={handleMatiereClick} isMatiere={true} />
-            ) : (
-              <div>Loading...</div>
-            )
+            <BackgroundWrapper>
+              {matieres ? (
+                <DisplayItems
+                  items={matieres}
+                  onClickItem={handleMatiereClick}
+                  isMatiere={true}
+                />
+              ) : (
+                <div>Loading...</div>
+              )}
+            </BackgroundWrapper>
           }
         />
         {/* Liens Utiles */}
-        <Route path="/liens-utiles/:lienUtileTitle" element={<LiensUtilesWithData />} />
-        <Route path="/liens-utiles" element={<LiensUtilesWithData />} />
+        <Route
+          path="/liens-utiles/:lienUtileTitle"
+          element={
+            <BackgroundWrapper>
+              <LiensUtilesWithData />
+            </BackgroundWrapper>
+          }
+        />
+        <Route
+          path="/liens-utiles"
+          element={
+            <BackgroundWrapper>
+              <LiensUtilesWithData />
+            </BackgroundWrapper>
+          }
+        />
         {/* Application de Randomisation */}
         {/* <Route path="/randomisation" element={<RandomisationComponent />} /> */}
         {/* Détails des cours */}
@@ -87,11 +114,15 @@ const HomePage = ({ fontSize }) => {
         {/* Sous-matières */}
         {sousMatieres &&
           sousMatieres.map((sousMatiere) => {
-            const Component = getComponentByActionType(sousMatiere.attributes.actionType);
+            const Component = getComponentByActionType(
+              sousMatiere.attributes.actionType
+            );
             const matiereTitleUrl = toUrlFriendly(
               sousMatiere.attributes.matiere.data.attributes.titre
             );
-            const sousMatiereTitleUrl = toUrlFriendly(sousMatiere.attributes.titre);
+            const sousMatiereTitleUrl = toUrlFriendly(
+              sousMatiere.attributes.titre
+            );
             return (
               <Route
                 key={sousMatiere.id}
@@ -101,7 +132,14 @@ const HomePage = ({ fontSize }) => {
             );
           })}
         {/* Matières */}
-        <Route path="/:matiereTitle" element={<Matiere />} />
+        <Route
+          path="/:matiereTitle"
+          element={
+            <BackgroundWrapper>
+              <Matiere />
+            </BackgroundWrapper>
+          }
+        />
       </Routes>
     </div>
   );
